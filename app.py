49f87d32b8acbdd285f2ef1819ed344c42434e9a
Eric Song <ericksong91@gmail.com>
2024-09-06 14:36:29 -0400

    Complete

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
 import urllib.request, json
 
 import os
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/movies")
 def get_movies():
-    url = "https://api.themoviedb.org/3/discover/movie?api_key={}".format(os.environ.get("TMDB_API_KEY"))
+    print("key: " + os.getenv("TMDB_API_KEY"))
+    url = "https://api.themoviedb.org/3/discover/movie?api_key={}".format(os.getenv("TMDB_API_KEY"))
 
     response = urllib.request.urlopen(url)
     data = response.read()
